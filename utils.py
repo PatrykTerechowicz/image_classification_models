a38c5d250cd5d57406b4e9e4d9bc77833c5add9f
Patryk Terechowicz <p.terechowicz@hemitech.pl>
2021-04-16 09:54:25 +0200

    Added transform used in models.

--- utils.py
+++ utils.py
@@ -1,6 +1,11 @@
 import torch.nn as nn
 import torch.data as data
 import torch
+from torchvision.transforms import Compose, ToTensor, Resize, Normalize
+
+
+def get_transform(target_size=224):
+    return Compose([Resize(target_size), ToTensor(), Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
 
 def get_model_device(model: nn.Module):
     """Returns device on which first parameters are located. We suppose that whole model is on same device.
